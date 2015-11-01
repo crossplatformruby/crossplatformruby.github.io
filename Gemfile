0d24606289df30b7bc748617c83736ef787e6d69
Michał Taszycki <michal@64bites.com>
2015-11-01 18:54:46 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source "http://rubygems.org"
+
+gem "middleman"
+gem "middleman-livereload"
+gem "middleman-deploy"
+gem "middleman-blog"
+gem "builder", "~> 3.0"
+gem "bourbon"
+gem "neat"
+
+gem "pry"
---
+++ config.rb
@@ -0,0 +1,43 @@
+require "extensions/views"
+require "lib/models/catalog"
+
+activate :views
+activate :directory_indexes
+# Time.zone = "UTC"
+
+page "/rss.xml", layout: false
+
+set :relative_links, true
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+set :layout, 'layouts/application'
+
+set :protocol, "http://"
+set :host, "crossplatformruby.com"
+set :port, 80
+
+configure :development do
+ activate :livereload
+end
+
+configure :build do
+  # Relative assets needed to deploy to Github Pages
+  activate :relative_assets
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch   = 'master'
+end
+
+helpers do
+end
+
+configure :development do
+  # Used for generating absolute URLs
+  set :host, "crossplatformruby-test.ngrok.com" #Middleman::PreviewServer.host
+  set :port, "80" #Middleman::PreviewServer.port
+end
